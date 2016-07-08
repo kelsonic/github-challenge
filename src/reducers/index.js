ab2d4fa069171cf49f52b32b1855ed06933546f1
kelsonic <kgadams93@gmail.com>
2016-07-08 15:55:23 -0700

    Omit comments and add comment to use rest api

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,6 @@
-/**
- * This is an empty reducer. You will need to define your
- * constants and actions, and have your reducer handle them
- * appropriately. You may wish to look at Redux's `componeReducers`
- * API if you wish to use reducer composition.
- *
- * For your actions, please use a standard FSA-compliant format (
- * a `type` property for the action's type and a `payload`, property
- * for the payload of the action).
- */
-
+// Ignore this, as user favorite repos will be saved via the REST API.
 const initialState = {
-  message: `This message is coming from the Redux store. You'll want to
-  remove this.`
+  favorites: []
 };
 
 export default function reducer(state = initialState, action) {
