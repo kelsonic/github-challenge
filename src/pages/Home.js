9f74baf8d3545cdbea447dba3399b7bb1c63080d
kelsonic <kgadams93@gmail.com>
2016-07-08 15:55:00 -0700

    Create HomePage class and have it render the SearchBar component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import SearchBar from '../components/SearchBar';
 
-function HomePage(props) {
-  return (
-    <div>
-      <h1>This is the home page.</h1>
-      <p>It is currently a stateless function. If you wish to use
-        any lifecycle methods here (which you likely will) you need to
-      make this a class component instead.</p>
-      <hr />
-      <p> {props.message} </p>
-    </div>
-  );
+export default class HomePage extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Search GitHub Repos to Favorite!</h1>
+        <SearchBar />
+      </div>
+    );
+  }
 }
-
-HomePage.propTypes = {
-  message: React.PropTypes.string
-};
-
-
-/**
- * As your Redux state gets larger, you'll want to avoid
- * passing the entire state object to your components when
- * they only need a slice of the state.
- */
-
-export default connect(state => state)(HomePage);
